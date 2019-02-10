3c5cc1d815906dd553aed76112e4c2b3eef31141
Andrew Craven <andrew@epworth-consulting.co.uk>
2019-02-10 20:34:20 +0000

    Add queue name attribute

---
+++ src/Grouchy.ServiceBus.Abstractions/QueueNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Grouchy.ServiceBus.Abstractions
+{
+   [AttributeUsage(AttributeTargets.Class)]
+   public class QueueNameAttribute : Attribute
+   {
+      public string Name { get; }
+
+      public QueueNameAttribute(string name)
+      {
+         Name = name;
+      }
+   }
+}
